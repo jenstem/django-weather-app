376eeeecb1d7391c2741c011cd7aa33e38125c82
Jen <124745888+jenstem@users.noreply.github.com>
2024-11-07 16:02:05 -0500

    Add docstring

--- weather_app/weather_project/apps.py
+++ weather_app/weather_project/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class WeatherProjectConfig(AppConfig):
+    """
+    Configuration class for teh Weather Project application.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'weather_project'
